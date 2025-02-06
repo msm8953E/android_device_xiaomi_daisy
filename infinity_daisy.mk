88fa4ef5a85f331a0eaa1a672a26597afdc4d32f
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-06 17:40:13 +0600

    Bring up

--- infinity_daisy.mk
+++ infinity_daisy.mk
@@ -13,17 +13,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 AB_OTA_UPDATER := true
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common infinity x stuff.
+$(call inherit-product, vendor/$(ROM_FOLDER)/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := daisy
-PRODUCT_NAME := lineage_daisy
+PRODUCT_NAME := $(ROM_FOLDER)_daisy
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 lite
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
+
+
+# Extra Stuffs
+INFINITY_MAINTAINER := FARHAN•AFK
+TARGET_BUILD_VIMUSIC := true
+USE_MOTO_CALCULATOR := true
+WITH_GMS := ture 
+ROM_FOLDER := infinity
+# Boot animation resolution.
+TARGET_BOOT_ANIMATION_RES := 720
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
