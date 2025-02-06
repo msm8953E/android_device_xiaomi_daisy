b163e6fe6a27996d0b64b780af13dcaecc00f8e8
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-06 17:47:10 +0600

    Phone info

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,3 +57,14 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 
 # Inherit the proprietary files
 include vendor/xiaomi/daisy/BoardConfigVendor.mk
+
+# missing something 
+ALLOW_MISSING_DEPENDENCIES=true
+
+# Missing Required Module
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true 
+ALLOW_MISSING_DEPENDENCIES := true 
+ALLOW_MISSING_DEPENDENCIES=true
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/Phoneinfo.prop
