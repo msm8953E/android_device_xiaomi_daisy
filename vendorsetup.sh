91e8bc6b24bd122c83e5452eedec3892aab6b6fa
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-09 00:58:10 +0600

    Yes

---
+++ vendorsetup.sh
@@ -0,0 +1,21 @@
+# ROM source patches
+
+color="\033[0;32m"
+end="\033[0m"
+
+echo -e "${color}Applying patches${end}"
+sleep 1
+
+
+# Kernel 
+ git clone https://github.com/xiaomi-msm8953-devs/android_kernel_xiaomi_msm8953 -b lineage-22.1 kernel/xiaomi/msm8953
+
+# Hardware 
+  git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.1 hardware/xiaomi
+  
+# Device common 
+ git clone https://github.com/xiaomi-msm8953-devs/android_device_xiaomi_msm8953 -b lineage-22.1 device/xiaomi/msm8953-common 
+
+# Vendor Blobs 
+git clone https://github.com/xiaomi-msm8953-devs/proprietary_vendor_xiaomi_daisy -b lineage-22.1 vendor/xiaomi/daisy
+git clone https://github.com/xiaomi-msm8953-devs/proprietary_vendor_xiaomi_msm8953 -b lineage-22.1 vendor/xiaomi/msm8953-common
