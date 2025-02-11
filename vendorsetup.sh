e6a99628f2443e0c0760cb48e6d00172d59e41cd
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-11 15:59:15 +0600

    Fingerprint added

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,3 +19,6 @@ sleep 1
 # Vendor Blobs 
 git clone https://github.com/xiaomi-msm8953-devs/proprietary_vendor_xiaomi_daisy -b lineage-22.1 vendor/xiaomi/daisy
 git clone https://github.com/xiaomi-msm8953-devs/proprietary_vendor_xiaomi_msm8953 -b lineage-22.1 vendor/xiaomi/msm8953-common
+
+# fingerprint 
+git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces -b lineage-21 vendor/fingerprint/opensource/interfaces
