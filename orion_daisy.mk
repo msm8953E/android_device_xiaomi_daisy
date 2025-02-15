20d8984d84b2fd8b48019ebcb690248c672b2e30
Gtajisan <ffjisan804@gmail.com>
2025-02-15 23:20:44 +0600

    bring up

---
+++ orion_daisy.mk
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit from daisy device
+AB_OTA_UPDATER := true
+$(call inherit-product, device/xiaomi/daisy/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/orion/config/common_full_phone.mk)
+7
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := daisy
+PRODUCT_NAME := orion_daisy
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi A2 lite
+PRODUCT_MANUFACTURER := Xiaomi
+WITH_GMS := false
+
+# GMS
+ORION_GAPPS :=  false 
+
+# OrionOS Flags
+ORION_BUILD_TYPE := Unofficial
+ORION_MAINTAINER := FARHAN • AFK
+ORION_MAINTAINER_LINK := https://t.me/FARHAN_MUH_TASIM
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="daisy-user 10 QKQ1.191002.002 V11.0.21.0.QDLMIXM release-keys" \
+    BuildFingerprint=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.21.0.QDLMIXM:user/release-keys
