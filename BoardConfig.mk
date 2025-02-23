6c798a87b783523f8d658f22c590fed966763f9e
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-02-23 14:40:19 +0600

    Fuck you

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,3 +57,12 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 
 # Inherit the proprietary files
 include vendor/xiaomi/daisy/BoardConfigVendor.mk
+
+
+# missing something 
+ALLOW_MISSING_DEPENDENCIES=true
+
+# Missing Required Module
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true 
+ALLOW_MISSING_DEPENDENCIES := true 
+ALLOW_MISSING_DEPENDENCIES=true
