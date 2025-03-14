df311dcbcd0aec4aee33b7c6b398c805a6f07035
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-14 19:55:07 +0600

    More App added

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,50 +5,103 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
-    Chrome \
-    Drive \
-    Gmail2 \
-    Maps \
-    BetterBugStub \
     AmbientSensePrebuilt \
-    arcore \
-    Camera2 \
+    AppDirectedSMSService \
+    BCR \
+    Chrome \
+    Chrome-Stub \
     ConnMO \
-    ConnMetrics \
+    CalendarGooglePrebuilt \
+    Drive \
     DCMO \
     DevicePolicyPrebuilt \
     DMService \
-    Drive \
-    GCS \
-    GoogleCamera \
-    Music \
+    MaestroPrebuilt \
+    Maps \
+    MicropaperPrebuilt \
     MyVerizonServices \
     OBDM_Permissions \
     obdm_stub \
-    OdadPrebuilt \
-    OemDmTrigger \
     OPScreenRecord \
     Ornament \
-    PixelLiveWallpaperPrebuilt \
-    PlayAutoInstallConfig \
     PrebuiltGmail \
+    PixelWallpapers2021 \
+    PixelWallpapers2020 \
     RecorderPrebuilt \
     SafetyHubPrebuilt \
     SCONE \
     ScribePrebuilt \
     Showcase \
     Snap \
-    Snap2 \
-    SnapdragonCamera \
     SoundAmplifierPrebuilt \
     SprintDM \
     SprintHM \
+    TurboPrebuilt \
+    Tycho \
+    TipsPrebuilt \
+    talkback \
     USCCDM \
-    Videos \
     VZWAPNLib \
     VzwOmaTrigger \
+    WallpapersBReel2020 \
     YouTube \
-    CalendarGooglePrebuilt
+    BetterBugStub \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+    Chrome-Stub \
+    GoogleContacts \
+    PrebuiltDeskClockGoogle \
+    PrebuiltBugle \
+    TrichromeLibrary \
+    TrichromeLibrary-Stub \
+    GCS \
+    WellbeingPrebuilt \
+    GoogleDialer \
+    DeviceIntelligenceNetworkPrebuilt \
+    DevicePersonalizationPrebuiltPixel2020 \
+    SafetyHubPrebuilt \
+    TurboPrebuilt \
+    HealthIntelligenceStubPrebuilt \
+    QuickSearchBox
+    AudioFX \
+    arcore \
+    AICorePrebuilt \
+    AiWallpapers \
+    AndroidAutoStubPrebuilt \
+    Camera2 \
+    Calendar2 \
+    Calendar \
+    DevicePersonalizationPrebuiltPixel2020 \
+    DeviceAsWebcam \
+    DuckDuckGo \
+    Etar \
+    Flash \
+    GoogleTTS \
+    Gallery2 \
+    Jellyfish \
+    Jelly \
+    Maps \
+    Photos \
+    PixelThemesStub \
+    PixelThemesStub2022_and_newer \
+    PixelWallpapers2023 \
+    PixelLiveWallpaperPrebuilt \
+    Recorder \
+    SnapCamera \
+    SafetyHubPrebuilt \
+    Seedvault \
+    SimpleGallery \
+    TurboPrebuilt \
+    WellbeingPrebuilt \
+    YouTube \
+    AdaptiveVPNPrebuilt-10307   \
+    GoogleTTS   \
+    SoundAmplifierPrebuilt_v4.7.638126989   \
+    SwitchAccessPrebuilt_1.15.0.629986523   \
+    talkback   \
+    Tycho   \
+    VoiceAccessPrebuilt   
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
