a932252340a75075d7d08c7d6f7f3a696ba685dd
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-03-25 04:08:32 +0600

    Axion bring up

--- lineage_daisy.mk
+++ lineage_daisy.mk
@@ -28,3 +28,11 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="daisy-user 10 QKQ1.191002.002 V11.0.21.0.QDLMIXM release-keys" \
     BuildFingerprint=xiaomi/daisy/daisy_sprout:10/QKQ1.191002.002/V11.0.21.0.QDLMIXM:user/release-keys
+
+
+# Misc extra 
+AXION_CAMERA_REAR_INFO := 12
+AXION_CAMERA_FRONT_INFO := 5
+AXION_MAINTAINER := FARHAN EXAM
+AXION_PROCESSOR := Qualcomm Snapdragon 625
+
