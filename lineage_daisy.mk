a219eecc0bf196c12df4c737e8dd9e14e5b62b03
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-04-11 18:30:41 +0600

    Daisy: added Gapps Ture*

--- lineage_daisy.mk
+++ lineage_daisy.mk
@@ -22,6 +22,7 @@ PRODUCT_NAME := lineage_daisy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A2 lite
 PRODUCT_MANUFACTURER := Xiaomi
+WITH_GMS := ture
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
