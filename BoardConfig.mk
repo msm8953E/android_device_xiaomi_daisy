d871ddf71925fbef8c2817eb1a6b3676d1bbf199
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-04-23 16:12:58 +0600

    Daisy: some missing stuff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,15 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 endif
 
 # Security Patch Level
-VENDOR_SECURITY_PATCH := 2021-07-01
+VENDOR_SECURITY_PATCH := 2021.07.01
 
 # Inherit the proprietary files
 include vendor/xiaomi/daisy/BoardConfigVendor.mk
+
+# missing something 
+ALLOW_MISSING_DEPENDENCIES=true
+
+# Missing Required Module
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true 
+ALLOW_MISSING_DEPENDENCIES := true 
+ALLOW_MISSING_DEPENDENCIES=true
