7932e1804c755fcb47ab8baba9378d5f535f92e2
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-05-03 16:33:37 +0600

    Daisy: risingOS Sakura

--- lineage_sakura.mk
+++ lineage_sakura.mk
@@ -15,6 +15,7 @@ AB_OTA_UPDATER := false
 $(call inherit-product, device/xiaomi/daisy/device.mk)
 
 # Inherit some common LineageOS stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
@@ -29,3 +30,40 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sakura-user 9 PKQ1.180917.001 V12.0.1.0.PDICNXM release-keys" \
     BuildFingerprint=xiaomi/sakura/sakura:9/PKQ1.180917.001/V12.0.1.0.PDICNXM:user/release-keys
+
+
+# Gapps.
+TARGET_DISABLES_GMS := true
+
+
+# Recognized ROM As.
+ROM_FOLDER := lineage
+
+# some extra 
+TARGET_DEFAULT_PIXEL_LAUNCHER := true/false 
+
+# Whether to ship prebuilt Google Dialer and Messages, false by default
+TARGET_INCLUDE_GOOGLE_DIALER := false
+
+# Add the following variables:
+
+# Lunch banner maintainer variable
+RISING_MAINTAINER="FARHAN"
+
+# Chipset/Maintainer properties (ro.rising.chipset/ro.rising.maintainer) 
+# Set RISING_MAINTAINER for version control 
+# (Optional if builder is setting properties via init_<device>.cpp)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="SDM625" \
+    RisingMaintainer="FARHAN"
+
+RISING_MAINTAINER := FARHAN
+
+# Disable/enable blur support, false by default
+TARGET_ENABLE_BLUR := false
+
+# Whether to ship aperture camera, false by default
+PRODUCT_NO_CAMERA := tfalse
+
+# Whether to ship lawnchair launcher, false by default
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := ture 
