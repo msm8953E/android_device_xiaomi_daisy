8d00104eeeba629c3998fb2292929a3e2333ca8b
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-05-04 06:11:55 +0600

    Added

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,3 +59,12 @@ VENDOR_SECURITY_PATCH := 2021-07-01
 
 # Inherit the proprietary files
 include vendor/xiaomi/daisy/BoardConfigVendor.mk
+
+
+# missing something 
+ALLOW_MISSING_DEPENDENCIES=true
+
+# Missing Required Module
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true 
+ALLOW_MISSING_DEPENDENCIES := true 
+BUILD_BROKEN_DUP_SYSPROP := true
