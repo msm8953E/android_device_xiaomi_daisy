78c43f6ed84d11c4e457533c2bd13557ffa0790a
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2025-06-03 12:30:47 +0600

    daisy: upgrade

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,8 +14,15 @@ sleep 1
   git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-22.2 hardware/xiaomi
   
 # Device common 
- git clone https://github.com/msm8953E/android_device_xiaomi_msm8953 -b lineage-22.2 device/xiaomi/msm8953-common 
+ git clone https://github.com/xiaomi-msm8953-devs/android_device_xiaomi_msm8953-common -b lineage-22.2 device/xiaomi/msm8953-common 
 
 # Vendor Blobs 
 git clone https://github.com/xiaomi-msm8953-devs/proprietary_vendor_xiaomi_daisy -b lineage-22.2 vendor/xiaomi/daisy
-git clone https://github.com/xiaomi-msm8953-devs/proprietary_vendor_xiaomi_msm8953 -b lineage-22.2 vendor/xiaomi/msm8953-common
+git clone https://github.com/xiaomi-msm8953-devs/proprietary_vendor_xiaomi_msm8953-common -b lineage-22.2 vendor/xiaomi/msm8953-common
+
+# fingerprint 
+git clone https://github.com/xiaomi-msm8953-devs/android_vendor_fingerprint_opensource_interfaces -b lineage-21 vendor/fingerprint/opensource/interfaces
+
+
+# Curl Signing Files
+git clone https://github.com/germaniumsculk/certs
